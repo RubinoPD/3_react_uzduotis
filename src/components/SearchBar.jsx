06a48a8798cece4da7f97fb4be687da5c779246f
RubinoPD <robertaskask@gmail.com>
2025-03-23 14:17:17 +0200

    Add SearcBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+function SearchBar({ searchTerm, onSearch, onClear }) {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Ieskoti pagal varda..."
+        value={searchTerm}
+        onChange={(e) => onSearch(e.target.value)}
+      />
+      {searchTerm && (
+        <button className="clear-button" onClick={onClear}>
+          Valyti paieska
+        </button>
+      )}
+    </div>
+  );
+}
